a5a4ceedb87135c5b8c511c24c277d01e147a453
kofston <lukkonop@icloud.com>
2019-07-13 09:34:10 +0200

    Add new files (price list is over)

---
+++ index.js
@@ -0,0 +1,23 @@
+// var topBar = new Vue({
+//     el: '#priceButton',
+//     data: {
+//         styleObject: {
+//             width: '0%'
+//           }
+//     },
+//     methods: {
+//         greet: function (event) {
+//             console.log('SIEMA!')
+//             new Vue({
+//                 el: '#PriceContent',
+//                 data: {
+//                     styleObject: {
+//                         width: '0%'
+//                       }
+//                 }
+//             });
+            
+//         }
+//     }
+// });
+
